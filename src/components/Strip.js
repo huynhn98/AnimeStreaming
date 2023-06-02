246c577ac02480314edba70460192140a17d2d41
huynhn98 <xqr482@my.utsa.edu>
2023-06-02 13:40:58 -0500

    change responsiveness for mobile

--- src/components/Strip.js
+++ src/components/Strip.js
@@ -21,8 +21,8 @@ const Strip = () => {
       },
       mobile: {
         breakpoint: { max: 464, min: 0 },
-        items: 1,
-        slidesToSlide: 1 // optional, default to 1.
+        items: 2,
+        slidesToSlide: 2 // optional, default to 1.
       }
     };
   
